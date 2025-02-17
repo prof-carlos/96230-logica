566939e1eb303eb3a3b39778319e4228cb8316cb
prof-carlos <128708765+prof-carlos@users.noreply.github.com>
2025-02-17 18:42:03 +0000

    Salvando

---
+++ 4. solicitando_dados.py
@@ -0,0 +1,10 @@
+import os
+os.system("clear")
+
+# Solicitando dados.
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+altura = float(input("Digite sua altura: "))
+
+# escreval("Nome: ", nome)
+print(f"Nome: {nome}")
