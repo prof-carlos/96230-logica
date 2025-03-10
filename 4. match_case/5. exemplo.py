091dcc2e0564ae2b503076a89ca82d40847f6b40
prof-carlos <128708765+prof-carlos@users.noreply.github.com>
2025-03-10 16:31:25 +0000

    save

--- 4. match_case/5. exemplo.py
+++ 4. match_case/5. exemplo.py
@@ -13,8 +13,26 @@ match forma_de_pagamento:
     case 1:
         # Aplicando desconto de 10%.
         desconto = valor_produto * 0.10
+        valor_pagar = valor_produto - desconto
+        
+        # Exibindo resultado.
+        print(f"\nValor do produto: R$ {valor_produto}")
+        print(f"Forma de pagamento: à vista")
+        print(f"Valor do desconto: R$ {desconto}")
+        print(f"Total da pagar: R$ {valor_pagar}")
     case 2:
-        ...
+        quantidade_parcelas = int(input("Digite a quantidade de parcelas: "))
+        if quantidade_parcelas >= 1 and quantidade_parcelas <= 6:
+            valor_parcela = valor_produto / quantidade_parcelas
+
+            # Exibindo resultado.
+            print(f"\nValor do produto: R$ {valor_produto}")
+            print(f"Forma de pagamento: à prazo")
+            print(f"Quantidade de parcelas: {quantidade_parcelas}")
+            print(f"Valor por parcela: R$ {valor_parcela:.2f}")
+            print(f"Total à prazo: R$ {valor_produto}")
+        else: 
+            print("O parcelamento deve ser em até 6 parcelas.")            
     case _:
         print("Opção inválida.")
 
