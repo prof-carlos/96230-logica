444a4c24c445e5d6b682686d30789d38b3a069bf
prof-carlos <128708765+prof-carlos@users.noreply.github.com>
2025-05-19 14:31:50 -0300

    save

--- 1. tipos de dados/1. ola_mundo.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.system("clear") # Limpa o terminal.
-
-print("Olá, mundo!") 
--- aula_laco_repeticao_for/2. com_laco.py
+++
@@ -1,9 +0,0 @@
-import os
-
-os.system("cls || clear")
-
-print("INÍCIO DO PROGRAMA")
-for i in range(1,6):
-    print(f"Valor da variável: {i}")
-
-print("FIM DO PROGRAMA")
